23e9c1dc165fceca4d1af27fc3292e4665311ece
Flankenshank <dylan.date@gmail.com>
2026-08-28 21:44:12 +0200

    First commit

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,14 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString  = await amqp.connect("amqp://guest:guest@localhost:5672");
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connected to RabbitMQ");
+  process.on("SIGINT", async () => {
+    console.log("\nShutting down...");
+    await conn.close();
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
