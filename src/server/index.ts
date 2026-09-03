7e30f3928f5e2b2e6ce46498ca18c01a1c97570b
Flankenshank <dylan.date@gmail.com>
2026-09-03 17:32:57 +0200

    Decoupling

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,31 @@
 import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
   const rabbitConn  = await amqp.connect("amqp://guest:guest@localhost:5672");
-  console.log("Connected to RabbitMQ");
-
   const channel = await rabbitConn.createConfirmChannel();
+  console.log("Connected to RabbitMQ");
+  printServerHelp();
+
+  while (true) {
+    const words = await getInput();
+    const command = words[0];
+    if (command === "pause") {
+      console.log("Sending a pause message.");
+      await publishJSON(channel, ExchangePerilDirect, PauseKey, { isPaused: true });
+    } else if (command === "resume") {
+      console.log("Sending a resume message.");
+      await publishJSON(channel, ExchangePerilDirect, PauseKey, { isPaused: false });
+    } else if (command === "quit") {
+      console.log("Exiting...");
+      break;
+    } else {
+      console.log(`Unknown command: ${command}`);
+    }
+
+  }
 
   process.on("SIGINT", async () => {
     console.log("\nShutting down...");
