1c3d256d4ccc37b89c292d659e382ce13db41dea
Flankenshank <dylan.date@gmail.com>
2026-09-05 22:27:35 +0200

    CH3 L7

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,10 @@
 import amqp from "amqplib";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
     const rabbitConn  = await amqp.connect("amqp://guest:guest@localhost:5672");
@@ -17,6 +20,38 @@ async function main() {
     console.log(`Username set to: ${username}`);
     const [, queue] = await declareAndBind(rabbitConn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient);
     console.log(`Queue ${queue.queue} declared and bound to exchange ${ExchangePerilDirect} with key ${PauseKey}`);
+
+    const gameState = new GameState(username);
+
+      while (true) {
+        const words = await getInput();
+        const command = words[0];
+        if (command === "spawn") {
+          try {
+            commandSpawn(gameState, words);
+          } catch (err) {
+            console.log((err as Error).message);
+          }
+        } else if (command === "move") {
+          try {
+            commandMove(gameState, words);
+          } catch (err) {
+          console.log((err as Error).message);
+        }
+      } else if (command === "status") {
+          commandStatus(gameState);
+        } else if (command === "help") {
+          printClientHelp();
+        } else if (command === "spam") {
+          console.log("Spamming not allowed yet!");
+        } else if (command === "quit") {
+          printQuit();
+          break;
+        } else {
+          console.log(`Unknown command: ${command}`);
+        }
+    
+      }
 
   }
 
