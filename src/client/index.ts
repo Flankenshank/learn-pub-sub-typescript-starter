0d03c538bdf372bd3ada06cd7e56c74e6dde72d1
Flankenshank <dylan.date@gmail.com>
2026-09-09 21:58:07 +0200

    added move

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,11 +1,13 @@
 import amqp from "amqplib";
-import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { ArmyMovesPrefix, ExchangePerilDirect, ExchangePerilTopic, PauseKey } from "../internal/routing/routing.js";
 import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
-import { declareAndBind, SimpleQueueType, subscribeJSON } from "../internal/pubsub/consume.js";
+import { SimpleQueueType, subscribeJSON } from "../internal/pubsub/consume.js";
 import { GameState } from "../internal/gamelogic/gamestate.js";
 import { commandSpawn } from "../internal/gamelogic/spawn.js";
 import { commandMove } from "../internal/gamelogic/move.js";
-import { handlerPause } from "./handlers.js";
+import { handlerPause, handlerMove } from "./handlers.js";
+import { publishJSON } from "../internal/pubsub/publish.js";
+import type { ArmyMove } from "../internal/gamelogic/gamedata.js";
 
 async function main() {
     const rabbitConn  = await amqp.connect("amqp://guest:guest@localhost:5672");
@@ -21,17 +23,36 @@ async function main() {
     console.log(`Username set to: ${username}`);
 
     const gameState = new GameState(username);
+    const publishCh = await rabbitConn.createConfirmChannel();
     
-    await subscribeJSON<{ isPaused: boolean }>(rabbitConn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient, handlerPause(gameState))
-    .then(() => {
+    try {
+    await subscribeJSON<{ isPaused: boolean }>(
+      rabbitConn, 
+      ExchangePerilDirect, 
+      `pause.${username}`,
+      PauseKey, 
+      SimpleQueueType.Transient,
+      handlerPause(gameState)
+    );
       console.log(`Subscribed to pause messages for user ${username}`);
-    })
-    .catch((err) => {
+    } catch(err) {
       console.error("Failed to subscribe to pause messages:", err);
-    });
+    };
+
+    const queueName = `${ArmyMovesPrefix}.${username}`;
+
+    try {
+      await subscribeJSON<ArmyMove>(rabbitConn, ExchangePerilTopic, queueName, `${ArmyMovesPrefix}.*`, SimpleQueueType.Transient, handlerMove(gameState))
+      console.log(`Subscribed to army moves for user ${username}`);
+    } catch(err) {
+      console.error("Failed to subscribe to army moves:", err);
+    };
 
       while (true) {
         const words = await getInput();
+        if (words.length === 0) {
+          continue;
+        }
         const command = words[0];
         if (command === "spawn") {
           try {
@@ -41,10 +62,12 @@ async function main() {
           }
         } else if (command === "move") {
           try {
-            commandMove(gameState, words);
+            const moveData = commandMove(gameState, words);
+            await publishJSON(publishCh, ExchangePerilTopic, `${ArmyMovesPrefix}.${username}`, moveData);
+            console.log(`Published move to ${moveData.toLocation} with ${moveData.units.length} unit(s)`);
           } catch (err) {
-          console.log((err as Error).message);
-        }
+            console.log((err as Error).message);
+          }
       } else if (command === "status") {
           commandStatus(gameState);
         } else if (command === "help") {
