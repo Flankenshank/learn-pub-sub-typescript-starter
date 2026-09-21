efa24c906b62de0157e0eb7d09a0c5a62f0cbe71
Flankenshank <dylan.date@gmail.com>
2026-09-21 21:17:22 +0200

    Add messagePack

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import { type ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -10,6 +11,29 @@ export function publishJSON<T>(
   const buffer = Buffer.from(jsonString, "utf-8");
   return new Promise((resolve, reject) => {
     ch.publish(exchange, routingKey, buffer, {contentType: "application/json"}, (err, ok) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+export function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const buffer = Buffer.from(encode(value));
+  return new Promise((resolve, reject) => {
+    ch.publish(
+      exchange, 
+      routingKey, 
+      buffer, 
+      {contentType: "application/x-msgpack"},
+      (err) => {
       if (err) {
         reject(err);
       } else {
